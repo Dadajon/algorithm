9704d236370f4cc19d4f6405a9a3f756b746dbf5
Dadajon Jurakuziev <dadajonjurakuziev@gmail.com>
2019-04-05 00:51:42 +0900

    Solved problem #1966

--- 1966/main.cpp
+++ 1966/main.cpp
@@ -26,6 +26,10 @@ int main() {
 
         sort(v.begin(), v.end());
 
+        for(int i : v)
+            cout << i << ' ';
+        cout << endl;
+
         while (!q.empty()) {
             if (q.front().first == v[v.size() - 1]) {
                 v.pop_back();
